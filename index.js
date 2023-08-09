d308ff981daabe66f7e75a7a7ad00d607c83a5e9
aryankarma <aryankarma29@gmail.com>
2023-08-10 00:31:30 +0530

    FAQ and carousel

---
+++ index.js
@@ -0,0 +1,11 @@
+var element = document.getElementsByClassName('wraptesti');
+
+function scrollNext(e){
+  console.log(e)
+  element.scrollLeft += 20;
+} 
+function scrollPrev(e){
+  console.log(e);
+  element.scrollLeft += 20;
+}
+
